9bf3f11ca0a028df83264dfb513379dba4567895
heleenderoo <heleen.deroo@inbo.be>
2023-07-18 06:11:11 +0200

    Adjust merge_duplicate_records so that MAN records are retained in case of conflict

--- src/functions/merge_duplicate_records.R
+++ src/functions/merge_duplicate_records.R
@@ -8,7 +8,9 @@
 #' separated by '_') to be evaluated
 #' @param merge Logical - Indicates whether records need to be merged (TRUE) or
 #' indicated using an extra column (FALSE)
-#'
+#' @param save_to_env Logical which indicates whether the output dataframe
+#' can be saved to the global environment and override any existing objects
+#' with the same name. Default is FALSE.
 #' @details
 #' For "so_som"
 #'
@@ -22,31 +24,31 @@
 #' reporting. Some countries reported both the mandatory and optional
 #' parameters, while others reported only the primary key and optional
 #' parameters. To address this issue, this function merges these duplicate
-#' records, as both rows usually contain data for different parameters (i.e.
-#' both rows are non-conflicting). The only potential loss of information is
-#' the difference in "date_labor_analyses". However, the most crucial date for
-#' is the sampling date.
+#' records, by taking the unique value over the two records per parameter.
+#' In case of conflicts (i.e. different non-NA value in MAN versus OPT for the
+#' given parameter), the value in MAN is retained, except for the "other_obs"
+#' column, in which the different values are pasted into one string (separated
+#' by a semicolon).
 #'
 #' @examples
 #' merge_duplicate_records("so_som", merge = TRUE)
 
-merge_duplicate_records <- function(survey_form, merge = NULL) {
+merge_duplicate_records <- function(survey_form,
+                                    merge = FALSE,
+                                    save_to_env = FALSE) {
 
-  # If no "merge" argument is provided, set it to "TRUE" (by default)
-
-if (is.null(merge)) {
-  merge <- TRUE
-  }
+  source("./src/functions/get_env.R")
+  source("./src/functions/assign_env.R")
 
   # Retrieve data form from the global environment
 
-df <- get(survey_form, envir = .GlobalEnv)
+df <- get_env(survey_form)
 
   # Store the number of initial rows
 
 nrow_initial <- nrow(df)
 
-# Four Latvian records (survey_year 2004; code_plot 2005) do not have
+# Four Latvian records (survey_year 2004; code_plot 5) do not have
 # code_layer nor layer limit information, and therefore have the same
 # unique_layer_repetition. However, based on "code_line", the records can
 # be grouped in two MAN-OPT pairs. This line updates unique_layer_repetition
@@ -65,12 +67,13 @@ if (length(which(duplicated(df$unique_layer_repetition[ind_latvia]))) == 3) {
   # Update unique_layer_repetition
 
   df$unique_layer_repetition[ind_latvia[ind_latvia_001]] <- "64_2004_5_001_1"
-  df$unique_layer_repetition[ind_latvia[ind_latvia_001]] <- "64_2004_5_002_1"
+  df$unique_layer_repetition[ind_latvia[ind_latvia_002]] <- "64_2004_5_002_1"
   }
 
 
 # List of unique_layer_repetition for which the combination of
 # unique_layer_repetition and the layer limits are duplicated
+
   # The Slovakian plot with double OL and different layer limits
   # (54_2007_208_OL_1) is not part of this, because that case is an
   # issue with the primary key/code_layer name
@@ -82,8 +85,10 @@ vec_dupl <- unique(df$unique_layer_repetition[which(duplicated(
 
 # Set up a progress bar to track processing
 
+if (!isTRUE(getOption("knitr.in.progress"))) {
 progress_bar <- txtProgressBar(min = 0,
                                max = length(vec_dupl), style = 3)
+}
 
 # Create lists of parameters for which different merge rules apply
 
@@ -93,30 +98,16 @@ progress_bar <- txtProgressBar(min = 0,
   # if conflicting values: take value for origin "MAN"
 
 list_man <- c("country", "partner_short", "partner", "survey_year",
-              "code_country", "code_plot", "code_layer", "repetition",
+              "code_country", "code_plot", "code_layer", 
+              "code_layer_original", "repetition",
               "layer_limit_superior", "layer_limit_inferior",
               "date_labor_analyses", "origin", "code_soil_horizon_sample_c",
               "partner_code", "q_flag", "change_date", "code_line", "line_nr",
               "qif_key", "download_date", "layer_type", "plot_id",
               "unique_survey", "unique_survey_repetition",
-              "unique_survey_layer", "unique_layer_repetition")
-
-  # Rule:
-  # if both values are the same: take the unique value,
-  # if there is only one non-NA: take non-NA value,
-  # if conflicting values: take value for origin "MAN" +
-  #                        indicate this in an extra column.
-
-list_man_alert <- c("subsamples", "code_texture_class")
-
-  # Rule:
-  # if both values are the same: take the unique value,
-  # if there is only one non-NA: take non-NA value,
-  # if conflicting values: take average +
-  #                        indicate this in an extra column
-  #                        (perhaps report the absolute difference too).
-
-list_avg_alert <- c("moisture_content", "part_size_clay", "part_size_silt",
+              "unique_survey_layer", "unique_layer_repetition",
+              "subsamples", "code_texture_class",
+              "moisture_content", "part_size_clay", "part_size_silt",
               "part_size_sand", "coarse_fragment_vol", "organic_layer_weight",
               "ph_cacl2", "ph_h2o", "organic_carbon_total", "n_total",
               "carbonates", "exch_acidiy", "exch_al", "exch_ca", "exch_fe",
@@ -129,11 +120,11 @@ list_avg_alert <- c("moisture_content", "part_size_clay", "part_size_silt",
               "exch_ace", "exch_cec", "elec_cond", "ni", "base_saturation",
               "p_ox", "bulk_density")
 
-# Rule:
-# if both values are the same: take the unique value,
-# if there is only one non-NA: take non-NA value,
-# if conflicting values: paste the value for the "MAN" record (first) +
-#                        then the value for the "OPT" record (second)
+  # Rule:
+  # if both values are the same: take the unique value,
+  # if there is only one non-NA: take non-NA value,
+  # if conflicting values: paste the value for the "MAN" record (first) +
+  #                        then the value for the "OPT" record (second)
 
 list_paste_alert <- c("other_obs")
 
@@ -143,17 +134,21 @@ source("./src/functions/as_character_summary.R")
 
 df_parameters <- names(df)
 df_parameters_extra <- df_parameters[which(!df_parameters %in%
-                                           c(list_man, list_man_alert,
-                                             list_avg_alert, list_paste_alert))]
+                                           c(list_man, list_paste_alert))]
 
 assertthat::assert_that(identical(df_parameters_extra, character(0)),
                         msg = paste0("The function does not know how to",
                                      " summarise the following columns: ",
                                      as_character_summary(df_parameters_extra)))
 
-# Add a row for the OPT records which should be removed after merging
+# Add a column for the OPT records which should be removed after merging
 
 df$to_remove <- NA
+
+# Add a column for the MAN records which will indicate later on whether a row
+# has been merged in the history
+
+df$origin_merged <- NA
 
 # Iterate over each double record
 for (i in seq_along(vec_dupl)) {
@@ -174,6 +169,11 @@ for (i in seq_along(vec_dupl)) {
 
   # Purpose: to complete the MAN row and remove the OPT row later on
 
+  # Add a column for the MAN records which will indicate later on whether a row
+  # has been merged in the history
+  
+  df$origin_merged[ind_man] <- TRUE
+
   # Indicate if row has to be removed
 
   df$to_remove[ind_opt] <- TRUE
@@ -184,7 +184,8 @@ for (i in seq_along(vec_dupl)) {
   # Extract the column names
   same_column_names <- names(df)[same_columns]
 
-  # Columns which need to be merged:
+  # Columns which need to be merged
+  # (i.e. for which MAN != OPT, or MAN or OPT is NA):
 
   df_pars_to_merge <- df_parameters[!df_parameters %in% same_column_names]
 
@@ -210,33 +211,6 @@ for (i in seq_along(vec_dupl)) {
         }
       }
 
-    # Parameters for which data in "MAN" row should be taken +
-    # contrasting data alert should be created
-
-    if (par_j %in% list_man_alert) {
-
-      if (any(!is.na(values_j)) && any(is.na(values_j))) {
-        summary_value <- values_j[which(!is.na(values_j))]
-        } else
-
-      if (all(!is.na(values_j)) && (length(unique(values_j)) == 2)) {
-        summary_value <- df[ind_man, col_ind_j]
-        }
-      }
-
-    # Parameters for which average of data should be taken
-
-    if (par_j %in% list_avg_alert) {
-
-      if (any(!is.na(values_j)) && any(is.na(values_j))) {
-        summary_value <- values_j[which(!is.na(values_j))]
-        } else
-
-      if (all(!is.na(values_j)) && (length(unique(values_j)) == 2)) {
-        summary_value <- mean(values_j)
-        }
-      }
-
     # Parameters for which contrasting data should be pasted together
 
     if (par_j %in% list_paste_alert) {
@@ -246,13 +220,10 @@ for (i in seq_along(vec_dupl)) {
         } else
 
       if (all(!is.na(values_j)) && (length(unique(values_j)) == 2)) {
-        summary_value <- paste0(df[ind_man, col_ind_j], "_",
+        summary_value <- paste0(df[ind_man, col_ind_j], "; ",
                                 df[ind_opt, col_ind_j])
         }
       }
-
-      # To do: create alert columns
-      # To do: check for variability (abs diff)
 
     # Paste the information in the MAN row
 
@@ -262,10 +233,15 @@ for (i in seq_along(vec_dupl)) {
   }
 
   # Update progress bar
+
+  if (!isTRUE(getOption("knitr.in.progress"))) {
   setTxtProgressBar(progress_bar, i)
+  }
 }
 
+if (!isTRUE(getOption("knitr.in.progress"))) {
 close(progress_bar)
+}
 
 # Determine the amount of rows to be removed
 # and the survey_years of these rows
@@ -284,40 +260,79 @@ if (identical(which(!is.na(df$to_remove)), integer(0))) {
 
 # If there are double records
 
+  # Add column with origin_merge_info
+  df$origin_merge_info <-
+    paste0("From ", nrow_initial, " to ",
+           nrow_final, " rows (range survey years: ",
+           min(years), " - ", max(years), ")")
+
   # If the duplicate rows have to be removed
   if (merge == TRUE) {
-
-    # Print the following:
-    cat(paste("Number of rows was reduced by",
-           rel_reduction,
-           "%, i.e. from", nrow_initial, "to", nrow_final, "rows.
-Records have been removed from survey years between",
-           min(years), "and", paste0(max(years), ".")))
 
     # Remove the OPT rows from the dataframe
     df <- df[-which(!is.na(df$to_remove)),
              -which(names(df) == "to_remove")]
 
+    if (save_to_env == TRUE) {
     # Save dataframe in global environment
-    assign(survey_form, df, envir = globalenv())
+    assign_env(survey_form, df)
+    }
+    
+    # Print the following:
+    cat(paste0("Number of rows was reduced by ",
+               rel_reduction,
+               " %, i.e. from ", nrow_initial, " to ", nrow_final, " rows.\n",
+               "Records have been removed from survey years between ",
+               min(years), " and ", max(years), ".\n"))
 
     } else {
 
   # If the duplicate rows do not have to be removed
 
+  if (save_to_env == TRUE) {
+  # Save dataframe in global environment
+  assign_env(survey_form, df)
+  }
 
   # Print the following:
-
-cat(paste("Suggestion to remove the number of rows by",
-            rel_reduction,
-            "%, i.e. from", nrow_initial, "to", nrow_final, "rows.
-Records to remove belong to the survey years between", min(years),
-           "and", paste0(max(years), ".")))
-
-  # Save dataframe in global environment
-  assign(survey_form, df, envir = globalenv())
+  cat(paste0("Suggestion to remove the number of rows by ",
+             rel_reduction,
+             " %, i.e. from ", nrow_initial, " to ", nrow_final, " rows.\n",
+             "Records to remove are indicated with TRUE in the column ",
+             "'to_remove', and belong to the survey years between ", min(years),
+             " and ", max(years), ".\n"))
 
     }
   }
 
+}
+
+
+
+extract_merge_info <- function(survey_form) {
+
+  source("./src/functions/get_env.R")
+
+  # Retrieve data form from the global environment
+  df <- get_env(survey_form)
+
+  # Extract origin_merge_info
+  origin_merge_info <- unique(df$origin_merge_info)
+  if (length(origin_merge_info) != 1) {
+    stop("origin_merge_info contains multiple unique values.")
+  }
+
+  # Extract the numeric values from origin_merge_info using regular expressions
+  nrow_initial <-
+    as.character(as.integer(str_extract(origin_merge_info, "\\d+")))
+  nrow_final <-
+    as.character(as.integer(str_extract(origin_merge_info, "\\d+(?= rows)")))
+  year_range <- str_extract(origin_merge_info, "(?<=survey years: )\\d+ - \\d+")
+  min_year <- as.character(as.numeric(str_extract(year_range, "\\d+")))
+  max_year <- as.character(as.numeric(str_extract(year_range, "(?<= - )\\d+")))
+
+  return(c(nrow_initial = nrow_initial,
+           nrow_final = nrow_final,
+           min_year = min_year,
+           max_year = max_year))
 }
