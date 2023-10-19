83039aad1d4f64faea54c585bc4ad1dadfd18677
heleenderoo <heleen.deroo@inbo.be>
2023-10-19 11:23:02 +0200

    Make "bind_inconsistency_lists" more general for other dataframe types

--- src/functions/bind_objects_starting_with.R
+++ src/functions/bind_objects_starting_with.R
@@ -5,7 +5,7 @@
 #' environment into a single data frame, and removes the original inconsistency
 #' lists from the environment.
 #'
-#' @param inconsistency_type Character string specifying the type of
+#' @param object_name_start Character string specifying the type of
 #' inconsistency lists to bind.
 #' @param save_to_env Logical indicating whether to save the combined data
 #' frame to the environment. Default is \code{FALSE}.
@@ -36,25 +36,33 @@
 #' # Bind the list of coordinate inconsistencies and return the combined data
 #' # frame
 #' bind_inconsistency_lists("list_primary_inconsistencies", save_to_env = TRUE)
+#' bind_inconsistency_lists(object_name_start = "pir_applied",
+#'                          object_type = "pir_applied",
+#'                          save_to_env = TRUE)
 #'
 #' @export
 
-bind_inconsistency_lists <- function(inconsistency_type, save_to_env = FALSE) {
+bind_objects_starting_with <- function(object_name_start,
+                                       object_type = "new_inconsistencies",
+                                       save_to_env = FALSE) {
 
   source("./src/functions/assign_env.R")
   source("./src/functions/get_date_local.R")
 
-  download_date <- get_date_local("./data/raw_data/")
-
-  assertthat::assert_that(inconsistency_type %in%
+  if (object_type == "new_inconsistencies") {
+    
+    download_date <- get_date_local("./data/raw_data/")
+    
+  assertthat::assert_that(object_name_start %in%
                             c("list_primary_inconsistencies",
                               "list_coordinate_inconsistencies",
                               "list_layer_inconsistencies",
                               "list_range_inconsistencies",
                               "list_derived_inconsistencies"),
                           msg = paste0("Unknown inconsistency type '",
-                                       inconsistency_type,
+                                       object_name_start,
                                        "' in input argument."))
+  }
 
   if (isTRUE(getOption("knitr.in.progress"))) {
     envir <- knitr::knit_global()
@@ -62,8 +70,28 @@ bind_inconsistency_lists <- function(inconsistency_type, save_to_env = FALSE) {
     envir <- globalenv()
   }
 
-  matching_objects <- ls(pattern = paste0("^", inconsistency_type, "_"),
+  list_data_tables <- list(so = c("som", "prf", "pls", "pfh", "lqa"),
+                           s1 = c("som", "prf", "pls", "pfh", "lqa"),
+                           si = c("eve", "plt", "sta", "tco"),
+                           y1 = c("pl1", "st1", "ev1"),
+                           sw = c("swa", "swc"))
+
+  list_data_tables <- unlist(
+    lapply(names(list_data_tables), function(name) {
+      paste0(name, "_", list_data_tables[[name]])
+    })
+  )
+  
+  matching_objects <- ls(pattern = paste0("^", object_name_start, "_"),
                          envir = envir)
+  
+  if (!identical(matching_objects, character(0))) {
+    
+  matching_objects <-
+    matching_objects[which(gsub(paste0(object_name_start, "_"),
+                                "",
+                                matching_objects) %in% list_data_tables)]
+  }
 
   if (!identical(matching_objects, character(0))) {
 
@@ -78,12 +106,15 @@ bind_inconsistency_lists <- function(inconsistency_type, save_to_env = FALSE) {
 
   if (save_to_env == TRUE) {
     rm(list = matching_objects, envir = envir)
-    assign_env(inconsistency_type, combined_data)
+    assign_env(object_name_start, combined_data)
   }
 
-  cat(paste0("'", inconsistency_type, "' contains ",
+  if (object_type == "new_inconsistencies") {
+    
+  cat(paste0("'", object_name_start, "' contains ",
              length(which(
                combined_data$non_duplicated_error_type_per_record == TRUE)),
              " unique inconsistencies.\n"))
+  }
 
 }
