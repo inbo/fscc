2accd72b677cd2291911c522ae58b8dea36b2c03
heleenderoo <heleen.deroo@inbo.be>
2023-11-21 13:32:33 +0100

    Use harmonise_layer_to_depths instead

--- src/specific_esb_requests/soil_fertility_data_for_ground_vegetation_plots.R
+++ src/specific_esb_requests/soil_fertility_data_for_ground_vegetation_plots.R
@@ -7,7 +7,7 @@
 # Script initiation date: 31 Oct 2023
 
 
-# Prepare required packages ----
+# 1. Prepare required packages ----
 
 stopifnot(require("sf"),
           require("tidyverse"),
@@ -17,7 +17,7 @@ stopifnot(require("sf"),
           require("googledrive"),
           require("assertthat"))
 
-# Import "layer 1" data for soil (Level II) ----
+# 2. Import "layer 1" data for soil (Level II) ----
 
 if (!exists("so_som")) {
 source("./src/functions/read_processed.R")
@@ -28,13 +28,11 @@ read_processed(survey_forms = c("si", "so"),
 so_som_working <- so_som %>%
   # Filter for records starting from 1996
   filter(.data$survey_year >= 1996) %>%
-  # Filter for records not deeper than 20 cm
-  # (10 cm is requested, but some plots only have a layer M02, e.g. "6_715")
-  filter(is.na(.data$layer_limit_inferior) |
-           .data$layer_limit_inferior <= 20)
+  # Remove redundant layers
+  filter(!is.na(.data$layer_number))
 
 
-# Import list of plots with ground vegetation data ----
+# 3. Import list of plots with ground vegetation data ----
 
 plots_vegetation <-
   read.csv(paste0("./data/additional_data/",
@@ -51,14 +49,15 @@ plots_vegetation <-
          ph_cacl2_forest_floor = NA,
          ph_h2o_forest_floor = NA)
 
-# Check: No issue with Polish plot codes
+# Assert: No issue with Polish plot codes
+
 assertthat::assert_that(
   min(plots_vegetation$code_plot[which(plots_vegetation$code_country == 53)]) >
     100)
 
 
 
-# Evaluate for each of the plot_ids ----
+# 4. Evaluate for each of the plot_ids ----
 
 # Set progress bar
 progress_bar <- txtProgressBar(min = 0,
@@ -73,119 +72,131 @@ for (i in seq_len(nrow(plots_vegetation))) {
   if (!identical(which(so_som_working$plot_id == plot_id_i),
                  integer(0))) {
   
-  ## 0 - 10 cm depth layer ----
   
-  # First: focus on layers M05/H05, M51/H51, M01/H01
+## 4.1. 0 - 10 cm depth layer ----
+  
+    # Some of the below-ground fixed-depth layers are not "conform"
+    # the theoretical depths (e.g. 4_503)
+    # Therefore, harmonise the layers covering the 0 - 10 cm depth range
     
-  suppressWarnings({
+    # Select the layers covering the 0 - 10 cm depth range
     
-  df_0_10 <- so_som_working %>%
-    filter(.data$plot_id == plot_id_i) %>%
-    filter(.data$layer_type %in% c("peat", "mineral")) %>%
-    filter(.data$layer_limit_inferior <= 10) %>%
-    # Aggregate per unique_survey_repetition (by taking a weighted average)
-    # In theory, there are two options:
-    # - Only M01/H01 appears: no weighted average needed
-    # - Both M05/H05 and M51/H51 appear: the two layers have the same
-    #   layer thickness (i.e. 5 cm), so weighting can happen based on
-    #   bulk density
-    group_by(unique_survey_repetition, plot_id) %>%
-    summarise(organic_carbon_total_avg =
-                ifelse(!is.na(.data$bulk_density),
-                       weighted.mean(organic_carbon_total,
-                                     w = bulk_density,
-                                     na.rm = TRUE),
-                       mean(organic_carbon_total, na.rm = TRUE)),
-              n_total_avg =
-                ifelse(!is.na(.data$bulk_density),
-                       weighted.mean(n_total,
-                                     w = bulk_density,
-                                     na.rm = TRUE),
-                       mean(n_total, na.rm = TRUE)),
-              ph_cacl2_avg =
-                ifelse(!is.na(.data$bulk_density),
-                       weighted.mean(ph_cacl2,
-                                     w = bulk_density,
-                                     na.rm = TRUE),
-                       mean(ph_cacl2, na.rm = TRUE)),
-              ph_h2o_avg =
-                ifelse(!is.na(.data$bulk_density),
-                       weighted.mean(ph_h2o,
-                                     w = bulk_density,
-                                     na.rm = TRUE),
-                       mean(ph_h2o, na.rm = TRUE)),
-              # max LOQs
-              .groups = "drop")
-  })
-  
-  # If any of the originally filtered profiles does not appear in the
-  # table above (df_0_10),
-  # which means that there are profiles with a layer M02/H02,
-  # add their info
-  
-  prof_orig <- so_som_working %>%
-    filter(.data$plot_id == plot_id_i) %>%
-    filter(.data$layer_type %in% c("peat", "mineral")) %>%
-    distinct(unique_survey_repetition)
-  
-  if (any(!prof_orig %in% df_0_10$unique_survey_repetition)) {
+    limit_range <- seq(10, 0)
     
-    df_0_20 <- so_som_working %>%
+    df_sub <- so_som_working %>%
       filter(.data$plot_id == plot_id_i) %>%
-      filter(.data$layer_type %in% c("peat", "mineral")) %>%
-      # only profiles that did not yet appear in df_0_10
-      filter(!unique_survey_repetition %in%
-               df_0_10$unique_survey_repetition) %>%
-      # Aggregate per unique_survey_repetition
+      filter(!is.na(layer_limit_superior) &
+               !is.na(layer_limit_inferior)) %>%
+      rowwise() %>%
+      filter(any(limit_range > .data$layer_limit_superior &
+                   limit_range < .data$layer_limit_inferior))
+    
+    # Apply the function harmonise_layer_to_depths.
+    # This function selects layers within the specified depth range
+    # and calculates a weighted average based on the soil mass
+    # contribution (overlapping depth range x bulk density) of each layer
+    # to the specified depth range.
+    
+    source("./src/functions/harmonise_layer_to_depths.R")
+    
+    df_0_10 <- df_sub %>%
       group_by(unique_survey_repetition, plot_id) %>%
-      summarise(organic_carbon_total_avg =
-                  ifelse(!is.na(.data$bulk_density),
-                         weighted.mean(organic_carbon_total,
-                                       w = bulk_density,
-                                       na.rm = TRUE),
-                         mean(organic_carbon_total, na.rm = TRUE)),
+      reframe(organic_carbon_total_avg =
+                  harmonise_layer_to_depths(limit_sup = 0,
+                                            limit_inf = 10,
+                                            bulk_density =
+                                              .data$bulk_density,
+                                            upper_depths =
+                                              .data$layer_limit_superior,
+                                            lower_depths =
+                                              .data$layer_limit_inferior,
+                                            variab =
+                                              .data$organic_carbon_total,
+                                            parameter_name =
+                                              "organic_carbon_total"),
                 n_total_avg =
-                  ifelse(!is.na(.data$bulk_density),
-                         weighted.mean(n_total,
-                                       w = bulk_density,
-                                       na.rm = TRUE),
-                         mean(n_total, na.rm = TRUE)),
+                  harmonise_layer_to_depths(limit_sup = 0,
+                                            limit_inf = 10,
+                                            bulk_density =
+                                              .data$bulk_density,
+                                            upper_depths =
+                                              .data$layer_limit_superior,
+                                            lower_depths =
+                                              .data$layer_limit_inferior,
+                                            variab =
+                                              .data$n_total,
+                                            parameter_name =
+                                              "n_total"),
                 ph_cacl2_avg =
-                  ifelse(!is.na(.data$bulk_density),
-                         weighted.mean(ph_cacl2,
-                                       w = bulk_density,
-                                       na.rm = TRUE),
-                         mean(ph_cacl2, na.rm = TRUE)),
+                  harmonise_layer_to_depths(limit_sup = 0,
+                                            limit_inf = 10,
+                                            bulk_density =
+                                              .data$bulk_density,
+                                            upper_depths =
+                                              .data$layer_limit_superior,
+                                            lower_depths =
+                                              .data$layer_limit_inferior,
+                                            variab =
+                                              .data$ph_cacl2,
+                                            parameter_name =
+                                              "ph_cacl2"),
                 ph_h2o_avg =
-                  ifelse(!is.na(.data$bulk_density),
-                         weighted.mean(ph_h2o,
-                                       w = bulk_density,
-                                       na.rm = TRUE),
-                         mean(ph_h2o, na.rm = TRUE)),
-                .groups = "drop")
-    
-    # Add the aggregated data of these profiles to the 0 - 10 cm profiles
-    
-    df_0_10 <- rbind(df_0_10,
-                     df_0_20)
-    
-  }
+                  harmonise_layer_to_depths(limit_sup = 0,
+                                            limit_inf = 10,
+                                            bulk_density =
+                                              .data$bulk_density,
+                                            upper_depths =
+                                              .data$layer_limit_superior,
+                                            lower_depths =
+                                              .data$layer_limit_inferior,
+                                            variab =
+                                              .data$ph_h2o,
+                                            parameter_name =
+                                              "ph_h2o"),
+                # LOQs
+                organic_carbon_total_loq =
+                  harmonise_layer_to_depths(limit_sup = 0,
+                                            limit_inf = 10,
+                                            bulk_density =
+                                              .data$bulk_density,
+                                            upper_depths =
+                                              .data$layer_limit_superior,
+                                            lower_depths =
+                                              .data$layer_limit_inferior,
+                                            variab =
+                                              .data$organic_carbon_total_loq,
+                                            parameter_name =
+                                              "organic_carbon_total_loq"),
+                n_total_loq =
+                  harmonise_layer_to_depths(limit_sup = 0,
+                                            limit_inf = 10,
+                                            bulk_density =
+                                              .data$bulk_density,
+                                            upper_depths =
+                                              .data$layer_limit_superior,
+                                            lower_depths =
+                                              .data$layer_limit_inferior,
+                                            variab =
+                                              .data$n_total_loq,
+                                            parameter_name =
+                                              "n_total_loq"),
+                .groups = "drop") %>%
+      # Aggregate per plot_id
+      group_by(plot_id) %>%
+      reframe(organic_carbon_total_avg =
+                  mean(organic_carbon_total_avg, na.rm = TRUE),
+                n_total_avg =
+                  mean(n_total_avg, na.rm = TRUE),
+                ph_cacl2_avg =
+                  mean(ph_cacl2_avg, na.rm = TRUE),
+                ph_h2o_avg =
+                  mean(ph_h2o_avg, na.rm = TRUE),
+                # LOQs
+                organic_carbon_total_loq =
+                  mean(organic_carbon_total_loq, na.rm = TRUE),
+                n_total_loq =
+                  mean(n_total_loq, na.rm = TRUE))
   
-  
-  suppressWarnings({
-  
-  df_0_10 <- df_0_10 %>%
-    # Aggregate per plot_id
-    group_by(plot_id) %>%
-    summarise(organic_carbon_total_avg =
-                mean(organic_carbon_total_avg, na.rm = TRUE),
-              n_total_avg =
-                mean(n_total_avg, na.rm = TRUE),
-              ph_cacl2_avg =
-                mean(ph_cacl2_avg, na.rm = TRUE),
-              ph_h2o_avg =
-                mean(ph_h2o_avg, na.rm = TRUE))
-  })
   
   # If any 0 - 10 cm layers were reported for the given plot_id
   
@@ -195,25 +206,29 @@ for (i in seq_len(nrow(plots_vegetation))) {
     ifelse(is.na(df_0_10$organic_carbon_total_avg) |
              is.na(df_0_10$n_total_avg),
            NA,
-           df_0_10$organic_carbon_total_avg / df_0_10$n_total_avg)
+           # If any value is below LOQ
+           ifelse((df_0_10$organic_carbon_total_avg <
+                     df_0_10$organic_carbon_total_loq) ||
+                    (df_0_10$n_total_avg < df_0_10$n_total_loq),
+                  -1,
+                  round(df_0_10$organic_carbon_total_avg / df_0_10$n_total_avg,
+                        2)))
   
-  plots_vegetation$ph_cacl2_0_10[i] <- df_0_10$ph_cacl2_avg
+  plots_vegetation$ph_cacl2_0_10[i] <- round(df_0_10$ph_cacl2_avg, 2)
   
-  plots_vegetation$ph_h2o_0_10[i] <- df_0_10$ph_h2o_avg
+  plots_vegetation$ph_h2o_0_10[i] <- round(df_0_10$ph_h2o_avg, 2)
   
   }
   
   
-  ## Forest floor ----
-  
-  suppressWarnings({
+## 4.2. Forest floor ----
   
   df_ff <- so_som_working %>%
     filter(.data$plot_id == plot_id_i) %>%
     filter(.data$layer_type == "forest_floor") %>%
     # Aggregate per unique_survey_repetition (by taking a weighted average)
     group_by(unique_survey_repetition, plot_id) %>%
-    summarise(organic_carbon_total_avg =
+    reframe(organic_carbon_total_avg =
                 ifelse(!is.na(.data$organic_layer_weight),
                        weighted.mean(organic_carbon_total,
                                      w = organic_layer_weight,
@@ -237,19 +252,36 @@ for (i in seq_len(nrow(plots_vegetation))) {
                                      w = organic_layer_weight,
                                      na.rm = TRUE),
                        mean(ph_h2o, na.rm = TRUE)),
+              # LOQs
+              organic_carbon_total_loq =
+                ifelse(!is.na(.data$organic_layer_weight),
+                       weighted.mean(organic_carbon_total_loq,
+                                     w = organic_layer_weight,
+                                     na.rm = TRUE),
+                       mean(organic_carbon_total_loq, na.rm = TRUE)),
+              n_total_loq =
+                ifelse(!is.na(.data$organic_layer_weight),
+                       weighted.mean(n_total_loq,
+                                     w = organic_layer_weight,
+                                     na.rm = TRUE),
+                       mean(n_total_loq, na.rm = TRUE)),
               .groups = "drop") %>%
     # Aggregate per plot_id
     group_by(plot_id) %>%
-    summarise(organic_carbon_total_avg =
+    reframe(organic_carbon_total_avg =
                 mean(organic_carbon_total_avg, na.rm = TRUE),
               n_total_avg =
                 mean(n_total_avg, na.rm = TRUE),
               ph_cacl2_avg =
                 mean(ph_cacl2_avg, na.rm = TRUE),
               ph_h2o_avg =
-                mean(ph_h2o_avg, na.rm = TRUE))
+                mean(ph_h2o_avg, na.rm = TRUE),
+              # LOQs
+              organic_carbon_total_loq =
+                mean(organic_carbon_total_loq, na.rm = TRUE),
+              n_total_loq =
+                mean(n_total_loq, na.rm = TRUE))
   
-  })
   
   # If any forest floor layers were reported for the given plot_id
   
@@ -259,11 +291,17 @@ for (i in seq_len(nrow(plots_vegetation))) {
     ifelse(is.na(df_ff$organic_carbon_total_avg) |
              is.na(df_ff$n_total_avg),
            NA,
-           df_ff$organic_carbon_total_avg / df_ff$n_total_avg)
+           # If any value is below LOQ
+           ifelse((df_0_10$organic_carbon_total_avg <
+                     df_0_10$organic_carbon_total_loq) ||
+                    (df_0_10$n_total_avg < df_0_10$n_total_loq),
+                  -1,
+                  round(df_ff$organic_carbon_total_avg / df_ff$n_total_avg,
+                        2)))
   
-  plots_vegetation$ph_cacl2_forest_floor[i] <- df_ff$ph_cacl2_avg
+  plots_vegetation$ph_cacl2_forest_floor[i] <- round(df_ff$ph_cacl2_avg, 2)
   
-  plots_vegetation$ph_h2o_forest_floor[i] <- df_ff$ph_h2o_avg
+  plots_vegetation$ph_h2o_forest_floor[i] <- round(df_ff$ph_h2o_avg, 2)
   
   }
   
@@ -279,10 +317,13 @@ plots_vegetation <- plots_vegetation %>%
   mutate_all(~ifelse(is.nan(.), NA, .))
 
 
-# Add eutric/dystric qualifier ----
+# 5. Add eutric/dystric qualifier ----
 
-suppressWarnings({
-  
+# This qualitative indicator is just a rough approximation, since the required
+# information is sometimes incomplete.
+# Basically, eutric/dystric qualifiers of a profile in WRB represent the
+# base saturation in the 20 - 100 cm depth range.
+
 so_prf_adds <-
   openxlsx::read.xlsx(paste0("./data/additional_data/",
                              "SO_PRF_ADDS.xlsx"),
@@ -299,18 +340,21 @@ so_prf_adds <-
                 .data$base_saturation_qualitative[!is.na(
                   .data$base_saturation_qualitative)]))))
 
-})
-
 plots_vegetation <- plots_vegetation %>%
   left_join(so_prf_adds,
             by = "plot_id") %>%
-  mutate(base_saturation_qualitative =
-           as.factor(.data$base_saturation_qualitative))
+  mutate(base_saturation_qualitative = case_when(
+    .data$base_saturation_qualitative == "low" ~ "< 50 %",
+    .data$base_saturation_qualitative == "high" ~ ">= 50 %",
+    TRUE ~ NA_character_))
 
 View(plots_vegetation)
 
+plot(plots_vegetation$c_to_n_0_10, plots_vegetation$c_to_n_forest_floor)
+colSums(!is.na(plots_vegetation))
 
-# Export ----
+
+# 6. Export ----
 
 write.csv2(plots_vegetation,
            paste0("./output/specific_esb_requests/",
