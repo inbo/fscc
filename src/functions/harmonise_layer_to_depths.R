3f38f7973e1dea5df2d011aa572d7b914fe9f6bc
heleenderoo <heleen.deroo@inbo.be>
2023-11-21 13:27:15 +0100

    Make function more general so that it is applicable to "som" next to "pfh"

--- src/functions/harmonise_layer_to_depths.R
+++ src/functions/harmonise_layer_to_depths.R
@@ -9,7 +9,23 @@
 #'
 #' @param limit_sup The upper depth limit of the wanted harmonisation range.
 #' @param limit_inf The lower depth limit of the wanted harmonisation range.
-#' @param df_sub_selected A data frame containing layer information.
+#' @param df_sub_selected A data frame containing layer information. Can be
+#' NULL, but then the parameters "bulk_density", "upper_depths", "lower_depths"
+#' and "variab" should not be NULL.
+#' @param bulk_density A vector containing bulk densities or organic layer
+#' weights which serve as weights. Should follow the same layer sequence
+#' as the parameters "upper_depths", "lower_depths", and "variab". 
+#' Should be NULL if df_sub_selected is not NULL.
+#' @param upper_depths A vector containing the upper layer limits.
+#' Should follow the same layer sequence as the parameters "bulk_density",
+#' "lower_depths", and "variab". Should be NULL if df_sub_selected is not NULL.
+#' @param lower_depths A vector containing the lower layer limits.
+#' Should follow the same layer sequence as the parameters "upper_depths",
+#' "bulk_density", and "variab". Should be NULL if df_sub_selected is not NULL.
+#' @param variab A vector containing the actual parameter values to be
+#' weighted. This vector can contain NAs. Should follow the same layer sequence
+#' as the parameters "upper_depths", "lower_depths", and "bulk_density". 
+#' Should be NULL if df_sub_selected is not NULL.
 #' @param parameter_name The name of the parameter to harmonise
 #' (e.g., "bulk_density").
 #' @param mode The mode for calculating harmonised values: "numeric"
@@ -32,13 +48,38 @@
 
 harmonise_layer_to_depths <- function(limit_sup,
                                       limit_inf,
-                                      df_sub_selected,
+                                      df_sub_selected = NULL,
+                                      bulk_density = NULL,
+                                      upper_depths = NULL,
+                                      lower_depths = NULL,
+                                      variab = NULL,
                                       parameter_name,
                                       mode = c("numeric",
                                                "categorical")) {
   
- 
   mode <- match.arg(mode)
+  
+  assertthat::assert_that(!is.null(df_sub_selected) ||
+                            (!is.null(bulk_density) &&
+                               !is.null(variab) &&
+                               !is.null(upper_depths) &&
+                               !is.null(lower_depths)))
+  
+  # If different vectors are given instead of a dataframe
+  # (this may be useful in a group_by() %>% summarise() construction)
+  
+  if (is.null(df_sub_selected)) {
+    
+    df_sub_selected <-
+      data.frame(bulk_density = bulk_density,
+                 horizon_limit_up = upper_depths,
+                 horizon_limit_low = lower_depths,
+                 variab = variab)
+    
+    names(df_sub_selected)[which(names(df_sub_selected) == "variab")] <-
+      parameter_name
+  }
+  
   
   # If any bulk density value is missing:
   # give all layers relatively the same bulk density weight
@@ -118,10 +159,6 @@ harmonise_layer_to_depths <- function(limit_sup,
     
     # Calculate the final value
     
-    # if (all(is.na(df_sub_selected[[parameter_name]]))) {
-    #   result <- NA
-    # } else {  
-    
     # If numeric: by taking the sum of the product
     
     if (mode == "numeric") {
@@ -139,12 +176,6 @@ harmonise_layer_to_depths <- function(limit_sup,
           pull(.data[[parameter_name]])
       }
   }
-  
-  # df_sub_selected %>% select(horizon_master,
-  #                            horizon_limit_low, horizon_limit_up,
-  #                            bulk_density)
-  #  , bd_gapfilled, weight_aid,
-  # weight)
   
   return(result)
   
