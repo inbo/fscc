e4fa4dc22c4d33d4f2bb2a8573a8745d90d1840d
heleenderoo <heleen.deroo@inbo.be>
2024-01-25 17:05:35 +0100

    Update copied column names to avoid issues with longitude and latitude columns

--- src/functions/as_sf.R
+++ src/functions/as_sf.R
@@ -28,26 +28,26 @@
 #' spatial_df <- as_sf(df)
 
 as_sf <- function(dataframe) {
-  
+
   assertthat::assert_that("longitude_dec" %in% names(dataframe) &&
                             "latitude_dec" %in% names(dataframe),
                           msg = paste0("Columns 'longitude_dec' and/or ",
                                        "'latitude_dec' do not exist in the ",
                                        "given dataframe."))
-  
+
   assertthat::assert_that(
     length(which(is.na(dataframe$longitude_dec))) == 0 &&
       length(which(is.na(dataframe$latitude_dec))) == 0,
     msg = paste0("Column(s) 'longitude_dec' and/or 'latitude_dec' contain(s) ",
                  "at least one NA."))
-  
+
   dataframe_spat <- dataframe %>%
-    mutate(longitude = longitude_dec) %>%
-    mutate(latitude = latitude_dec) %>%
-    st_as_sf(coords = c("longitude", "latitude"),
+    mutate(longitude_dec_sf = longitude_dec) %>%
+    mutate(latitude_dec_sf = latitude_dec) %>%
+    st_as_sf(coords = c("longitude_dec_sf", "latitude_dec_sf"),
              crs = 4326) %>%
     st_transform(crs = 3035)
-  
+
   return(dataframe_spat)
-  
+
 }
