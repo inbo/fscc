addb3c6fd8e1ba61f4afd82a6e6de3959cbc8664
heleenderoo <heleen.deroo@inbo.be>
2024-04-18 18:31:06 +0200

    Place pedotransfer functions in a separate script

--- src/functions/gapfill_with_predictions.R
+++ src/functions/gapfill_with_predictions.R
@@ -37,56 +37,16 @@ gapfill_with_predictions <- function(survey_form,
   }
 
 
-  # Pedotransfer function for bulk density in mineral layers ----
+  # Pedotransfer function for bulk density ----
 
-  # Expected relationship (BD-PTF) with TOC:
-  # BD = 1511 – (81,1*sqrt(TOC))
-
-  # This is based on the analysis of layer 1 "s1" data (using different models
-  # and predictors) and validation using the layer 1 "so" data
-  # (in Feb/March 2024) - cfr. script [TO DO: ADD SCRIPT]
-
-  bd_ptf <- function(toc) {
-    ifelse(is.na(toc), NA, 1511 - (81.1 * sqrt(toc)))
-  }
-
-
-  # Mean bulk density for forest floor
-
-  source("./src/functions/get_bulk_density_stats.R")
-
-  bd_ff <- get_bulk_density_stats(mode = "mean",
-                                  layer_type = "forest_floor")
-
-  # Mean bulk density for peat
-
-  source("./src/functions/get_bulk_density_stats.R")
-
-  bd_peat <- get_bulk_density_stats(mode = "mean",
-                                    layer_type = "peat")
+  source("./src/functions/bulk_density_ptf.R")
 
 
 
 
-  # Uncertainty in PTF ----
 
-  uncertainty_ptf <- 160 # kg m-3
 
-  bd_ptf_upper <- function(bd) {
-    ifelse(is.na(bd),
-           NA,
-           ifelse((round(bd + uncertainty_ptf) > 2650),
-                  2650,
-                  round(bd + uncertainty_ptf)))
-  }
 
-  bd_ptf_lower <- function(bd) {
-    ifelse(is.na(bd),
-           NA,
-           ifelse((round(bd - uncertainty_ptf) < 0),
-                  0,
-                  round(bd - uncertainty_ptf)))
-  }
 
 
   #  Gap-fill bulk densities for mineral layers ----
