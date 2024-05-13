e6161b3d1baa1dc1e7d23e1460a133efc7ad5e26
heleenderoo <heleen.deroo@inbo.be>
2024-05-13 12:43:56 +0200

    Include upper and lower ranges for pedotransfer organic layers

--- src/functions/bulk_density_ptf.R
+++ src/functions/bulk_density_ptf.R
@@ -72,6 +72,8 @@ bd_ff <- get_parameter_stats(parameter = "bulk_density",
                              mode = "stat",
                              layer_type = "forest_floor_excl_ol")
 
+bd_ff_upper_95 <- bd_ff[which(names(bd_ff) == "95%")]
+bd_ff_lower_5 <- bd_ff[which(names(bd_ff) == "5%")]
 bd_ff <- bd_ff[which(names(bd_ff) == "Mean")]
 
 
@@ -83,8 +85,9 @@ bd_peat <- get_parameter_stats(parameter = "bulk_density",
                                mode = "stat",
                                layer_type = "peat")
 
+bd_peat_upper_95 <- bd_peat[which(names(bd_peat) == "95%")]
+bd_peat_lower_5 <- bd_peat[which(names(bd_peat) == "5%")]
 bd_peat <- bd_peat[which(names(bd_peat) == "Mean")]
-
 
 
 
