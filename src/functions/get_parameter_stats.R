b5c9e266e696be841a21b818d6dfd2f5e9485ea9
heleenderoo <heleen.deroo@inbo.be>
2024-08-07 08:31:25 +0200

    Add data source as input parameter

--- src/functions/get_parameter_stats.R
+++ src/functions/get_parameter_stats.R
@@ -16,6 +16,11 @@
 #'                   Possible values are "organic" (default), "forest_floor",
 #'                   "forest_floor_excl_ol", "peat" and "mineral",
 #'                   "ol", "of", "oh".
+#' @param data_source A character string specifying the source data to use
+#'                    for the parameter statistics. Options include "LI",
+#'                    "LII" and "LI_and_LII". Default is "LI", since Level I
+#'                    is the systematic network and therefore assumed to be
+#'                    representative for Europe.
 #' @param quantile A numeric value indicating the quantile range
 #'                (default is 95).
 #' @param hist_plausible A logical indicating whether a histogram has to be
@@ -39,6 +44,7 @@
 get_parameter_stats <- function(parameter = "bulk_density",
                                 mode = "median",
                                 layer_type = "organic",
+                                data_source = "LI",
                                 quantile = 95,
                                 print = FALSE,
                                 hist_plausible = FALSE) {
@@ -56,6 +62,7 @@ get_parameter_stats <- function(parameter = "bulk_density",
   # Retrieve the dataframes ----
 
   empty_df <- data.frame(
+    plot_id = NA_character_,
     code_layer = NA_character_,
     survey_year = NA_integer_,
     layer_limit_superior = NA_integer_,
@@ -67,184 +74,232 @@ get_parameter_stats <- function(parameter = "bulk_density",
 
   ## s1_som ----
 
-  if (exists("s1_som", envir = .GlobalEnv)) {
+  if (data_source %in% c("LI", "LI_and_LII")) {
 
-    df_s1_som <- get_env("s1_som")
+    if (exists("s1_som", envir = .GlobalEnv)) {
 
-    if (par_source %in% names(df_s1_som)) {
+      df_s1_som <- get_env("s1_som")
+
+      if (par_source %in% names(df_s1_som)) {
+        df_s1_som <- df_s1_som %>%
+          filter(!(grepl("pfh|swc", .data[[par_source]])))
+      }
+
+      if (parameter == "bulk_density") {
+
+        df_s1_som <- df_s1_som %>%
+          mutate(
+            # Layer thickness
+            layer_thickness =
+              ifelse(!is.na(.data$layer_limit_superior) &
+                       !is.na(.data$layer_limit_inferior) &
+                       (.data$layer_limit_superior !=
+                          .data$layer_limit_inferior),
+                     abs(.data$layer_limit_superior -
+                           .data$layer_limit_inferior),
+                     NA_real_),
+            # Bulk density based on layer weight
+            # to compare with plausibility range of bulk density
+            bulk_density_layer_weight =
+              ifelse(!is.na(.data$organic_layer_weight) &
+                       (.data$organic_layer_weight != -1) &
+                       !is.na(.data$layer_thickness) &
+                       (.data$layer_type %in% c("forest_floor", "peat")),
+                     # kg m-3
+                     .data$organic_layer_weight /
+                       (.data$layer_thickness * 1e-2),
+                     NA_real_)) %>%
+          # Assumption: bulk densities derived from organic layer weights
+          # are generally more reliable than normal bulk densities in organic
+          # layers
+          mutate(bulk_density_harm =
+                   coalesce(bulk_density_layer_weight, bulk_density)) %>%
+          select(-bulk_density) %>%
+          rename(bulk_density = bulk_density_harm)
+      }
+
       df_s1_som <- df_s1_som %>%
-        filter(!(grepl("pfh|swc", .data[[par_source]])))
+        rename("parameter" = any_of(parameter))
+
+    } else {
+
+      df_s1_som <- empty_df
+      cat("\n's1_som' does not exist in Global Environment.\n")
+
     }
 
-    if (parameter == "bulk_density") {
-
-    df_s1_som <- df_s1_som %>%
-      mutate(
-        # Layer thickness
-        layer_thickness =
-          ifelse(!is.na(.data$layer_limit_superior) &
-                   !is.na(.data$layer_limit_inferior) &
-                   (.data$layer_limit_superior !=
-                      .data$layer_limit_inferior),
-                 abs(.data$layer_limit_superior -
-                       .data$layer_limit_inferior),
-                 NA_real_),
-        # Bulk density based on layer weight
-        # to compare with plausibility range of bulk density
-        bulk_density_layer_weight =
-          ifelse(!is.na(.data$organic_layer_weight) &
-                   (.data$organic_layer_weight != -1) &
-                   !is.na(.data$layer_thickness) &
-                   (.data$layer_type %in% c("forest_floor", "peat")),
-                 # kg m-3
-                 .data$organic_layer_weight / (.data$layer_thickness * 1e-2),
-                 NA_real_)) %>%
-      # Assumption: bulk densities derived from organic layer weights
-      # are generally more reliable than normal bulk densities in organic
-      # layers
-      mutate(bulk_density_harm =
-               coalesce(bulk_density_layer_weight, bulk_density)) %>%
-      select(-bulk_density) %>%
-      rename(bulk_density = bulk_density_harm)
-    }
-
-    df_s1_som <- df_s1_som %>%
-      rename("parameter" = any_of(parameter))
 
   } else {
 
     df_s1_som <- empty_df
-    cat("\n's1_som' does not exist in Global Environment.\n")
-
   }
+
 
   ## s1_pfh ----
 
-  if (exists("s1_pfh", envir = .GlobalEnv)) {
+  if (data_source %in% c("LI", "LI_and_LII")) {
 
-    df_s1_pfh <- get_env("s1_pfh")
+    if (exists("s1_pfh", envir = .GlobalEnv)) {
 
-    if (!"bulk_density" %in% names(df_s1_pfh)) {
+      df_s1_pfh <- get_env("s1_pfh")
+
+      if (!"bulk_density" %in% names(df_s1_pfh)) {
+        df_s1_pfh <- df_s1_pfh %>%
+          mutate(bulk_density = horizon_bulk_dens_measure)
+      }
+
       df_s1_pfh <- df_s1_pfh %>%
-        mutate(bulk_density = horizon_bulk_dens_measure)
-    }
+        rename(organic_carbon_total = horizon_c_organic_total) %>%
+        rename(code_layer = horizon_master) %>%
+        rename("parameter" = any_of(parameter))
 
-    df_s1_pfh <- df_s1_pfh %>%
-      rename(organic_carbon_total = horizon_c_organic_total) %>%
-      rename(code_layer = horizon_master) %>%
-      rename("parameter" = any_of(parameter))
+      if (par_source %in% names(df_s1_pfh)) {
+        df_s1_pfh <- df_s1_pfh %>%
+          filter(!(grepl("som|swc", .data[[par_source]])))
+      }
 
-    if (par_source %in% names(df_s1_pfh)) {
-      df_s1_pfh <- df_s1_pfh %>%
-        filter(!(grepl("som|swc", .data[[par_source]])))
+
+    } else {
+
+      df_s1_pfh <- empty_df
+      cat("\n's1_pfh' does not exist in Global Environment.\n")
+
     }
 
 
   } else {
 
     df_s1_pfh <- empty_df
-    cat("\n's1_pfh' does not exist in Global Environment.\n")
-
   }
+
 
   ## so_som ----
 
-  if (exists("so_som", envir = .GlobalEnv)) {
+  if (data_source %in% c("LII", "LI_and_LII")) {
 
-    df_so_som <- get_env("so_som")
+    if (exists("so_som", envir = .GlobalEnv)) {
 
-    if (par_source %in% names(df_so_som)) {
+      df_so_som <- get_env("so_som")
+
+      if (par_source %in% names(df_so_som)) {
+        df_so_som <- df_so_som %>%
+          filter(!(grepl("pfh|swc", .data[[par_source]])))
+      }
+
+      if (parameter == "bulk_density") {
+
+        df_so_som <- df_so_som %>%
+          mutate(
+            # Layer thickness
+            layer_thickness =
+              ifelse(!is.na(.data$layer_limit_superior) &
+                       !is.na(.data$layer_limit_inferior) &
+                       (.data$layer_limit_superior !=
+                          .data$layer_limit_inferior),
+                     abs(.data$layer_limit_superior -
+                           .data$layer_limit_inferior),
+                     NA_real_),
+            # Bulk density based on layer weight
+            # to compare with plausibility range of bulk density
+            bulk_density_layer_weight =
+              ifelse(!is.na(.data$organic_layer_weight) &
+                       (.data$organic_layer_weight != -1) &
+                       !is.na(.data$layer_thickness) &
+                       (.data$layer_type %in% c("forest_floor", "peat")),
+                     # kg m-3
+                     .data$organic_layer_weight /
+                       (.data$layer_thickness * 1e-2),
+                     NA_real_)) %>%
+          # Assumption: bulk densities derived from organic layer weights
+          # are generally more reliable than normal bulk densities in organic
+          # layers
+          mutate(bulk_density_harm =
+                   coalesce(bulk_density_layer_weight, bulk_density)) %>%
+          select(-bulk_density) %>%
+          rename(bulk_density = bulk_density_harm)
+      }
+
       df_so_som <- df_so_som %>%
-        filter(!(grepl("pfh|swc", .data[[par_source]])))
+        rename("parameter" = any_of(parameter))
+
+
+    } else {
+
+      df_so_som <- empty_df
+      cat("\n'so_som' does not exist in Global Environment.\n")
     }
-
-    if (parameter == "bulk_density") {
-
-      df_so_som <- df_so_som %>%
-        mutate(
-          # Layer thickness
-          layer_thickness =
-            ifelse(!is.na(.data$layer_limit_superior) &
-                     !is.na(.data$layer_limit_inferior) &
-                     (.data$layer_limit_superior !=
-                        .data$layer_limit_inferior),
-                   abs(.data$layer_limit_superior -
-                         .data$layer_limit_inferior),
-                   NA_real_),
-          # Bulk density based on layer weight
-          # to compare with plausibility range of bulk density
-          bulk_density_layer_weight =
-            ifelse(!is.na(.data$organic_layer_weight) &
-                     (.data$organic_layer_weight != -1) &
-                     !is.na(.data$layer_thickness) &
-                     (.data$layer_type %in% c("forest_floor", "peat")),
-                   # kg m-3
-                   .data$organic_layer_weight / (.data$layer_thickness * 1e-2),
-                   NA_real_)) %>%
-        # Assumption: bulk densities derived from organic layer weights
-        # are generally more reliable than normal bulk densities in organic
-        # layers
-        mutate(bulk_density_harm =
-                 coalesce(bulk_density_layer_weight, bulk_density)) %>%
-        select(-bulk_density) %>%
-        rename(bulk_density = bulk_density_harm)
-    }
-
-    df_so_som <- df_so_som %>%
-      rename("parameter" = any_of(parameter))
-
 
   } else {
 
     df_so_som <- empty_df
-    cat("\n'so_som' does not exist in Global Environment.\n")
   }
+
+
 
   ## so_pfh ----
 
-  if (exists("so_pfh", envir = .GlobalEnv)) {
+  if (data_source %in% c("LII", "LI_and_LII")) {
 
-    df_so_pfh <- get_env("so_pfh")
+    if (exists("so_pfh", envir = .GlobalEnv)) {
 
-    if (!"bulk_density" %in% names(df_so_pfh)) {
+      df_so_pfh <- get_env("so_pfh")
+
+      if (!"bulk_density" %in% names(df_so_pfh)) {
+        df_so_pfh <- df_so_pfh %>%
+          mutate(bulk_density = horizon_bulk_dens_measure)
+      }
+
       df_so_pfh <- df_so_pfh %>%
-        mutate(bulk_density = horizon_bulk_dens_measure)
+        rename(organic_carbon_total = horizon_c_organic_total) %>%
+        rename(code_layer = horizon_master) %>%
+        rename("parameter" = any_of(parameter))
+
+      if (par_source %in% names(df_so_pfh)) {
+        df_so_pfh <- df_so_pfh %>%
+          filter(!(grepl("som|swc", .data[[par_source]])))
+      }
+
+
+    } else {
+
+      df_so_pfh <- empty_df
+      cat("\n'so_pfh' does not exist in Global Environment.\n")
     }
-
-    df_so_pfh <- df_so_pfh %>%
-      rename(organic_carbon_total = horizon_c_organic_total) %>%
-      rename(code_layer = horizon_master) %>%
-      rename("parameter" = any_of(parameter))
-
-    if (par_source %in% names(df_so_pfh)) {
-      df_so_pfh <- df_so_pfh %>%
-        filter(!(grepl("som|swc", .data[[par_source]])))
-    }
-
 
   } else {
 
     df_so_pfh <- empty_df
-    cat("\n'so_pfh' does not exist in Global Environment.\n")
   }
 
 
-  if (parameter == "bulk_density") {
 
-  # sw_swc
 
-  if (exists("sw_swc", envir = .GlobalEnv)) {
 
-    df_sw_swc <- get_env("sw_swc") %>%
-      rename(code_layer = code_depth_layer) %>%
-      rename("parameter" = any_of(parameter))
+  ## sw_swc ----
+
+  if (data_source %in% c("LII", "LI_and_LII")) {
+
+    if (parameter == "bulk_density") {
+
+      if (exists("sw_swc", envir = .GlobalEnv)) {
+
+        df_sw_swc <- get_env("sw_swc") %>%
+          rename(code_layer = code_depth_layer) %>%
+          rename("parameter" = any_of(parameter))
+
+      } else {
+
+        df_sw_swc <- empty_df
+        cat("\n'sw_swc' does not exist in Global Environment.\n")
+      }
+    }
 
   } else {
 
     df_sw_swc <- empty_df
-    cat("\n'sw_swc' does not exist in Global Environment.\n")
   }
-  }
+
+
 
 
 
@@ -254,19 +309,19 @@ get_parameter_stats <- function(parameter = "bulk_density",
   data_complete <- bind_rows(
     # s1_som
     df_s1_som %>%
-      select(layer_type, code_layer, survey_year, parameter),
+      select(plot_id, layer_type, code_layer, survey_year, parameter),
 
     # s1_pfh
     df_s1_pfh %>%
-      select(layer_type, code_layer, survey_year, parameter),
+      select(plot_id, layer_type, code_layer, survey_year, parameter),
 
     # so_som
     df_so_som %>%
-      select(layer_type, code_layer, survey_year, parameter),
+      select(plot_id, layer_type, code_layer, survey_year, parameter),
 
     # so_pfh
     df_so_pfh %>%
-      select(layer_type, code_layer, survey_year, parameter))
+      select(plot_id, layer_type, code_layer, survey_year, parameter))
 
   if (parameter == "bulk_density") {
 
@@ -274,14 +329,32 @@ get_parameter_stats <- function(parameter = "bulk_density",
       data_complete,
       # sw_swc
       df_sw_swc %>%
-        select(layer_type, code_layer, survey_year, parameter))
+        select(plot_id, layer_type, code_layer, survey_year, parameter))
   }
+
+
+  # Filter out NAs
 
   data_complete <- data_complete %>%
     filter(!is.na(parameter)) %>%
     arrange(parameter)
 
 
+
+  # Obtain one value per code_layer per plot_id x survey_year
+  # (so that each plot contributes to the same extent to the average)
+
+  # Note that this is not yet the ideal approach, since this should ideally be
+  # based on a depth match rather than a code_layer match
+
+  data_complete <- data_complete %>%
+    # Filter to keep only the most recent survey year for each plot_id
+    group_by(plot_id) %>%
+    filter(survey_year == max(survey_year)) %>%
+    ungroup() %>%
+    # Take average per code_layer
+    group_by(plot_id, code_layer, layer_type) %>%
+    reframe(parameter = mean(parameter, na.rm = TRUE))
 
 
 
