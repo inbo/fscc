f610fbeee03ec2b3c3bbd682d6e41684fa3e21b9
heleenderoo <heleen.deroo@inbo.be>
2025-08-08 11:32:14 +0200

    Add columns shorter_name_cap, full_name, unit_stock_markdown

--- src/functions/get_par_table.R
+++ src/functions/get_par_table.R
@@ -51,37 +51,50 @@ get_par_table <- function() {
       "%wt", "kg m-3", "mg kg-1", "mg kg-1",
       "kg m-2"),
     # Only for variables for which stocks can be calculated
-    # unit_density_per_cm = c(
-    #   "kg ha-1 cm-1", "kg ha-1 cm-1", NA, NA, "1E4 mol+ ha-1 cm-1",
-    #   "t ha-1 cm-1", "kg ha-1 cm-1", "1E4 mol+ ha-1 cm-1",
-    #   "1E4 mol+ ha-1 cm-1",
-    #   "kg ha-1 cm-1",
-    #   "1E4 mol+ ha-1 cm-1", NA, "t ha-1 cm-1", "kg ha-1 cm-1",
-    #   "1E4 mol+ ha-1 cm-1",
-    #   "kg ha-1 cm-1", "1E4 mol+ ha-1 cm-1", "kg ha-1 cm-1", "t ha-1 cm-1",
-    #   "1E4 mol+ ha-1 cm-1",
-    #   "kg ha-1 cm-1", "kg ha-1 cm-1", "kg ha-1 cm-1", NA, NA,
-    #   "kg ha-1 cm-1", "1E4 mol+ ha-1 cm-1", "kg ha-1 cm-1", "kg ha-1 cm-1",
-    #   "kg ha-1 cm-1",
-    #   "kg ha-1 cm-1", "kg ha-1 cm-1", "kg ha-1 cm-1", "kg ha-1 cm-1",
-    #   "kg ha-1 cm-1",
-    #   "kg ha-1 cm-1", "kg ha-1 cm-1", NA, "1E4 mol+ ha-1 cm-1",
-    #   "kg ha-1 cm-1",
-    #   "kg ha-1 cm-1", "kg ha-1 cm-1", "kg ha-1 cm-1",
-    #   NA, NA, "kg ha-1 cm-1", "kg ha-1 cm-1",
-    #   NA),
-    # Only for variables for which stocks can be calculated
     shorter_name = c(
       "extrac_pb", "extrac_zn", NA, NA, "exch_mn",
       "caco3", "extrac_al", "exch_mg", "exch_fe", "extrac_na",
       "exch_ca", NA, "n", "extrac_k", "exch_al",
-      "tot_mg", "exch_k", "extrac_s", "c", "exch_na",
+      "tot_mg", "exch_k", "extrac_s", "oc", "exch_na",
       "extrac_cr", "extrac_fe", "tot_k", NA, NA,
       "extrac_cu", "free_h", "extrac_cd", "extrac_hg", "extrac_ca",
       "extrac_mg", "extrac_ni", "extrac_p", "extrac_al", "extrac_mn",
-      "extrac_fe", "tot_ca", NA, "exch_acidiy", "tot_na",
+      "extrac_fe", "tot_ca", NA, NA, "tot_na",
       "tot_mn", "tot_fe", "tot_al",
       NA, NA, "rea_al", "rea_fe",
+      NA
+    ),
+    # Shorter name capital (for in units)
+    # Only for variables for which stocks can be calculated
+    shorter_name_cap = c(
+      "Pb", "Zn", NA, NA, "Mn",
+      "CaCO3", "Al", "Mg", "Fe", "Na",
+      "Ca", NA, "N", "K", "Al",
+      "Mg", "K", "S", "C", "Na",
+      "Cr", "Fe", "K", NA, NA,
+      "Cu", "H", "Cd", "Hg", "Ca",
+      "Mg", "Ni", "P", "Al", "Mn",
+      "Fe", "Ca", NA, NA, "Na",
+      "Mn", "Fe", "Al",
+      NA, NA, "Al", "Fe",
+      NA
+    ),
+    full_name = c(
+      "Extractable lead", "Extractable zinc", NA, NA, "Exchangeable manganese",
+      "Carbonates", "Extractable aluminium", "Exchangeable magnesium",
+      "Exchangeable iron",  "Extractable sodium",
+      "Exchangeable calcium", NA, "Nitrogen", "Extractable potassium",
+      "Exchangeable aluminium",
+      "Total magnesium", "Exchangeable potassium", "Extractable sulfur",
+      "Organic carbon", "Exchangeable sodium",
+      "Extractable chromium", "Extractable iron", "Total potassium", NA, NA,
+      "Extractable cupper", "Free H+ acidity",
+      "Exctractable cadmium", "Extractable mercury", "Extractable calcium",
+      "Extractable magnesium", "Extractable nickel", "Extractable phosphorus",
+      "Extractable aluminium", "Extractable manganese",
+      "Extractable iron", "Total calcium", NA, NA, "Total sodium",
+      "Total manganese", "Total iron", "Total aluminium",
+      NA, NA, "Reactive aluminium", "Reactive iron",
       NA
     )) %>%
     mutate(
@@ -116,9 +129,26 @@ get_par_table <- function() {
         gsub("1E4", "10<sup>4</sup>",
              gsub("-1", "<sup>-1</sup>",
                   gsub("-2", "<sup>-2</sup>",
-                       gsub("-3", "<sup>-3</sup>", unit_density_per_cm)))))
+                       gsub("-3", "<sup>-3</sup>", unit_density_per_cm)))),
+      unit_stock_markdown =
+        case_when(
+          # Only calculate for rows for which stocks can be calculated (i.e.
+          # where shorter_var_name is not NA)
+          !is.na(shorter_name) & unit == "mg kg-1" ~
+            paste0("kg ", shorter_name_cap, " ha<sup>-1</sup>"),
+            #"kg ha-1",
+          # Important: this needs to be converted!!!!!
+          !is.na(shorter_name) & unit == "cmol+ kg-1" ~
+            paste0("kg ", shorter_name_cap, " ha<sup>-1</sup>"),
+           # "kg ha-1",
+          !is.na(shorter_name) & unit == "g kg-1" ~
+            paste0("t ", shorter_name_cap, " ha<sup>-1</sup>"),
+           # "t ha-1",
+          TRUE ~ NA_character_
+        ))
 
 
   return(parameter_table)
 
 }
+
